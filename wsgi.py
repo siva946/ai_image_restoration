73e1832a1bd36e906f084b5beb5edb11792742d9
siva946 <123248824+siva946@users.noreply.github.com>
2025-06-17 00:54:34 +0530

    uploaded files

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-from app import app
-
-if __name__ == "__main__":
-    app.run()
+version https://git-lfs.github.com/spec/v1
+oid sha256:853039c6c169dd0c940e46c5d2e9e8c55be52f49bbc951db76391d90193a1471
+size 64
