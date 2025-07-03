0d78fa1c03f9e9cfd529cb58c57c0f64a530ae51
siva946 <123248824+siva946@users.noreply.github.com>
2025-07-03 06:32:49 +0530

    Final

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:853039c6c169dd0c940e46c5d2e9e8c55be52f49bbc951db76391d90193a1471
-size 64
+from app import app
+
+if __name__ == "__main__":
+    app.run()
